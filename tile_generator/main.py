42acbd7763f62d125ecc1a1118a8f180dd167741
umittadelen <72702832+umit-09@users.noreply.github.com>
2023-06-02 12:08:10 +0200

    Update main.py

--- tile_generator/main.py
+++ tile_generator/main.py
@@ -52,7 +52,7 @@ for filename in glob.iglob(root_dir + '*.png', recursive=True):
             tile_count += 1
             current_tile = 0
 
-    print(f"\033[0;31mSet ID:\033[0m \033[0;32m{tile_count}\033[0m  \033[0;31mCurrent image:\033[0m \033[0;32m{filename.split('./item\\')}\033[0m")
+    print(f"\033[0;31mSet ID:\033[0m \033[0;32m{tile_count}\033[0m  \033[0;31mCurrent image:\033[0m \033[0;32m{filename.split('./item')[1]}\033[0m")
 
 # Save the final image if there are remaining tiles
 if current_tile > 0:
