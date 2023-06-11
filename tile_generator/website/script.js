653c18f647cccddfd6975d0dcfe294bf886ebb24
umittadelen <72702832+umit-09@users.noreply.github.com>
2023-06-11 13:12:10 +0200

    Revert "Summary"
    This reverts commit 9c7560c2e842610d64e8d9ed5809203d1a3a9c25.

--- tile_generator/website/script.js
+++
@@ -1,8 +0,0 @@
-const element = document.getElementById("myBtn");
-element.addEventListener("click", myFunction);
-
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Hello World";
-    var x = document.getElementById("myAudio");
-    x.play();
-}
